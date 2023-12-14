61ddb120f9f4df951f6ea0b3f73a045d5aba7757
CtrlAltDelight <lbchigges@gmail.com>
2023-12-14 12:06:58 -0500

    Processes one character at a time now

--- src/frequencies.rs
+++ src/frequencies.rs
@@ -1,10 +1,16 @@
 use std::fs;
+use std::io::{self, Read, BufReader};
 
 pub type Frequencies = [u64; 256];
 
-pub fn calc_frequencies(freqs: &mut Frequencies, path: &str) {
-    let content = fs::read_to_string(path).expect("Could not read the file.");
-    for byte in content.bytes() {
+pub fn calc_frequencies(freqs: &mut Frequencies, path: &str) -> io::Result<()>{
+    let file = fs::File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for byte_result in reader.bytes() {
+        let byte: u8 = byte_result?;
         freqs[byte as usize] += 1;
     }
+
+    Ok(())
 }
--- tests/test_frequencies.rs
+++ tests/test_frequencies.rs
@@ -3,9 +3,10 @@ mod tests {
     fn it_works() {
         use huffmancli::frequencies::{calc_frequencies, Frequencies};
         let mut frequencies: Frequencies = [0; 256];
-        calc_frequencies(&mut frequencies, "./tests/frequencies.txt");
+        let _ = calc_frequencies(&mut frequencies, "./tests/frequencies.txt");
         assert_eq!(frequencies['a' as usize], 3);
         assert_eq!(frequencies['b' as usize], 2);
         assert_eq!(frequencies['c' as usize], 1);
+        assert_eq!(frequencies['\n' as usize], 1);
     }
 }
